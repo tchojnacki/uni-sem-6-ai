9181b332f3bee4650dae46c5f6f778d37b64965a
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2023-03-09 19:43:16 +0100

    Test currently implemented features in main

--- path-finding/src/main.rs
+++ path-finding/src/main.rs
@@ -1,3 +1,17 @@
+use crate::{pos::PosConverter, time::Time};
+
+mod pos;
+mod time;
+mod vec3;
+
 fn main() {
-    println!("Hello, world!");
+    let pc = PosConverter::initialize();
+    let p0 = pc.wgs84_to_pos("51.16042707", "17.12241711");
+    let p1 = pc.wgs84_to_pos("51.16201253", "17.12469012");
+    let p2 = pc.wgs84_to_pos("51.11038700", "17.03102025");
+
+    println!("Rynek: {p2:?}");
+    println!("Odległość na Zakrzowie: {:.3} km", p0.distance_km(p1));
+
+    println!("{}", Time::from("00:15:00"));
 }
