46ba89ef6a945b00af5cadb06dfe831ae3dc5883
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2023-03-23 11:29:48 +0100

    Add option to run Dijkstra from CLI

--- path-finding/src/bin/astar.rs
+++ path-finding/src/bin/astar.rs
@@ -1,5 +1,6 @@
 use path_finding::{
-    astar_buses, astar_dist, astar_time, display, read_line, BusNetwork, StopHeuristic, Time,
+    astar_buses, astar_dist, astar_time, dijkstra_time, display, read_line, BusNetwork,
+    StopHeuristic, Time,
 };
 
 const EXPECTED_SPEED_KMPH: f32 = 10.;
@@ -12,7 +13,7 @@ fn main() {
 
     let start_name = read_line("Podaj przystanek początkowy A: ");
     let end_name = read_line("Podaj przystanek końcowy B: ");
-    let criteria = read_line("Podaj kryterium optymalizacyjne [t/p/d]: ");
+    let criteria = read_line("Podaj kryterium optymalizacyjne [t/p/d/dt]: ");
     let start_time = Time::from(read_line("Podaj czas początkowy (np. \"00:00:00\"): ").as_str());
 
     match criteria.as_str() {
@@ -31,6 +32,7 @@ fn main() {
             STOP_HEURISTIC,
         )),
         "d" => display(astar_dist(&bn, &start_name, start_time, &end_name)),
+        "dt" => display(dijkstra_time(&bn, &start_name, start_time, &end_name)),
         _ => panic!("Błędne kryterium optymalizacyjne!"),
     }
 }
